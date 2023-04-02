1a59c0ecef98dd231c44004dc235eb1cbdc5a82a
jakeer678 <jakeersasanoor@gmail.com>
2023-04-02 22:59:27 +0530

    updated

--- src/store/ContextProvider.js
+++ src/store/ContextProvider.js
@@ -4,14 +4,25 @@ import { userContext } from "./ContextStore";
 const ContextProvider = (props) => {
   const initialValue = localStorage.getItem("idToken");
   const [token, setToken] = useState(initialValue);
-    
+
   const LoginUserHandle = (idToken) => {
     setToken(idToken);
-    localStorage.setItem("idToken", idToken)
+    localStorage.setItem("idToken", idToken);
+  };
+
+  const LogoutHandler = () => {
+    setToken(null);
+    localStorage.clear();
+  };
+
+  const contextValue = {
+    token: token,
+    LoginUserHandle: LoginUserHandle,
+    LogoutHandler: LogoutHandler,
   };
 
   return (
-    <userContext.Provider value={{ LoginUserHandle }}>
+    <userContext.Provider value={{ contextValue }}>
       {props.children}
     </userContext.Provider>
   );
