f2e83df4f4aca9edda25f946270d95509bc7c429
jakeer678 <jakeersasanoor@gmail.com>
2023-04-04 20:39:27 +0530

    updated

--- src/components/PagesRoute/ForgotPassword.js
+++ src/components/PagesRoute/ForgotPassword.js
@@ -1,10 +1,44 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./Forgot.css";
+import { Link, useNavigate } from "react-router-dom";
 
 const ForgotPassword = () => {
   const emailInputRef = useRef();
-  const passwordInputRef = useRef();
-  const newPasswordSubmitHandler = () => {};
+  const [isLoading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const newPasswordSubmitHandler = async (e) => {
+    e.preventDefaut();
+    const enteredEmail = emailInputRef.current.value;
+    setLoading(true);
+    try {
+      const response = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyCu5UWll7yrSyYvqmDYmYLdxlWNkCixilI",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            requestType: "PASSWORD_RESET",
+            email: enteredEmail,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      setLoading(false);
+      const responseData = await response.json();
+      console.log(responseData, "forgotPassword");
+      if (responseData.error) {
+        alert(responseData.error.message);
+      } else {
+        alert("Check your email inbox and reset password");
+        navigate("/");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <>
       <div>
@@ -19,25 +53,11 @@ const ForgotPassword = () => {
             />
           </div>
           <div>
-            <input
-              type="password"
-              placeholder="New-Password"
-              className="form-control"
-              required
-              ref={passwordInputRef}
-            />
-          </div>
-          <div>
-            <input
-              type="password"
-              placeholder="confirm New-Password"
-              className="form-control"
-              required
-              ref={passwordInputRef}
-            />
-          </div>
-          <div>
-            <button>Submit</button>
+            {!isLoading && <button type="submit">Send Link</button>}
+            {isLoading && <p>sending request</p>}
+            <p>
+              Already a user?<Link to="/login">Login</Link>
+            </p>
           </div>
         </form>
       </div>
